665bf1df1f2e5ce7c62ca9a6dd2cd1283d47e509
kaiyuanshi <kaiyuanshi@tencent.com>
2024-12-20 00:20:20 +0800

    lcs

--- lcs/lcs.go
+++ lcs/lcs.go
@@ -1,3 +1,60 @@
 package lcs
 
+import "gopkg/util"
+
 // longest common subsequence
+
+func lengthOfLCS(a, b string) int {
+	dp := make([][]int, len(a)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(b)+1)
+	}
+
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			if a[i-1] == b[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = util.Max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(a)][len(b)]
+}
+
+func findLCS(a, b string) string {
+	dp := make([][]int, len(a)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(b)+1)
+	}
+
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			if a[i-1] == b[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = util.Max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	i, j := len(a), len(b)
+	var lcs []byte
+	for i > 0 && j > 0 {
+		if a[i-1] == b[j-1] {
+			lcs = append(lcs, a[i-1])
+			i--
+			j--
+		} else if dp[i-1][j] > dp[i][j-1] {
+			i--
+		} else {
+			j--
+		}
+	}
+	var rlcs []byte
+	for i := len(lcs) - 1; i >= 0; i-- {
+		rlcs = append(rlcs, lcs[i])
+	}
+	return string(rlcs)
+}
---
+++ lcs/lcs_test.go
@@ -0,0 +1,41 @@
+package lcs
+
+import (
+	"testing"
+)
+
+func Test_lengthOfLCS(t *testing.T) {
+	tests := []struct {
+		name string
+		a    string
+		b    string
+		want int
+	}{
+		{name: "normal1", a: "abcde", b: "aceb", want: 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLCS(tt.a, tt.b); got != tt.want {
+				t.Errorf("lengthOfLCS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findLCS(t *testing.T) {
+	tests := []struct {
+		name string
+		a    string
+		b    string
+		want string
+	}{
+		{name: "normal1", a: "abcde", b: "aceb", want: "ace"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findLCS(tt.a, tt.b); got != tt.want {
+				t.Errorf("findLCS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
